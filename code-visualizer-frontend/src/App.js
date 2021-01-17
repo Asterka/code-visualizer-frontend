ef740074d939b6f68a4032d378cbbe02485b647a
asterka <askar.master@yandex.ru>
2021-01-17 17:33:59 +0300

    Added core functionality, working with the localhost

---
+++ code-visualizer-frontend/src/App.js
@@ -0,0 +1,12 @@
+import FileUpload from "./FileUpload";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FileUpload/>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
